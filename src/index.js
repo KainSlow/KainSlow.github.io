63b357b33e03d6b32e3f7f8becee134eca6912e0
KainSlow <jorge_alfonso3@hotmail.com>
2025-07-22 14:10:37 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,94 @@
 
 document.addEventListener("DOMContentLoaded", async()=>{
-    let panel = document.querySelector(".panel");
-
+    
     const requestURL = "/assets/data/info.json"
     const request = new Request(requestURL);
     const response = await fetch(request);
-    const myInfo = await response.json();
+    const info = await response.json();
+    
+    let panel = document.querySelector(".panel");
+    let title = document.querySelector("#title");
 
-    let buttons = document.querySelectorAll("button");
-    buttons[0].addEventListener("click", (e)=>{
+    let aboutme = document.querySelector("#aboutme");
+    let gamedev_btns = document.querySelectorAll(".gamedev-btn");
+    aboutme.addEventListener("click", (e)=>{
         panel.innerHTML = "";
-        displayInfo(panel, myInfo.about);
+        createAbout(panel, info);
     });
-    buttons[1].addEventListener("click", (e)=>{
+
+    gamedev_btns[0].addEventListener("click",()=>{
+        title.innerHTML = "Generación procedural";
         panel.innerHTML = "";
-        displayInfo(panel, myInfo.gamedev);
-        createSlider(panel, myInfo.gamedevImgs);
+        createSlider(panel, info.pcgImgs);
+        displayInfo(panel, info.pcg);
     });
-    /**
-     * 
-    buttons[2].addEventListener("click", (e)=>{
+    gamedev_btns[1].addEventListener("click",()=>{
+        title.innerHTML = "Pathfinding";
         panel.innerHTML = "";
-        displayInfo(panel, myInfo.webdev);
+        createSlider(panel, info.pathImgs);
+        displayInfo(panel, info.pathfinding);
     });
-    */
-    displayInfo(panel, myInfo.about);
+    gamedev_btns[2].addEventListener("click",()=>{
+        title.innerHTML = "Prototipos";
+        panel.innerHTML = "";
+        createSlider(panel, info.protoImgs);
+        displayInfo(panel, info.protos);
+        addCustomProtoInfo(panel);
+    });
+    
+    //createAbout(panel, info);
+    title.innerHTML = "Prototipos";
+    panel.innerHTML = "";
+    createSlider(panel, info.protoImgs);
+    displayInfo(panel, info.protos);
+    addCustomProtoInfo(panel);
+
 })
 
+
+function createAbout(panel, info){
+    let title = document.querySelector("#title");
+    title.innerHTML = "Sobre mi";
+    displayInfo(panel, info.about);
+    createSlider(panel, info.pixelartImgs);
+    document.querySelector(".slider").style.aspectRatio = "4/3";
+}
+
+function addCustomProtoInfo(panel){
+    let t1 = document.createElement("h3");
+    t1.appendChild(document.createTextNode("Deeper Down (1ra imágen)"));
+    panel.appendChild(t1);
+    let p1 = document.createElement("p");
+    let p1_2 = document.createElement("p");
+    p1.appendChild(document.createTextNode("Videojuego en el cuál eres un científico en busca de nuevas especies de seres vivos en un planeta desconocido, tu objetivo es completar misiones mientras te mantienes con vida. Versión jugable disponible en mi página de Itch.io"));
+    p1_2.appendChild(document.createTextNode("Para realizar el comportamiento de los peces utilicé el modelo de simulación \"Boids\", el cual permite definir patrones de comportamiento que seguirá un grupo de entidades, usualmente con el objetivo de conseguir un movimiento similar al natural."));
+    panel.appendChild(p1);
+    panel.appendChild(p1_2);
+
+    let t2 = document.createElement("h3");
+    t2.appendChild(document.createTextNode("Humberstone (2da imágen)"));
+    panel.appendChild(t2);
+    let p2 = document.createElement("p");
+    let p2_2 = document.createElement("p");
+    p2.appendChild(document.createTextNode("Eres un trabajador de la antigua mina de salitre Humberstone, estás harto de realizar trabajos forzados y recibir tu paga en fichas, sin mencionar los malos tratos de tus jefes, por lo que te propones buscar la manera de escapar. Versión jugable disponible en mi página de Itch.io"));
+    p2_2.appendChild(document.createTextNode("El prototipo más \"completo\" que he hecho, tiene sistema de progresión, interacción básica con NPCs, tienda, diferentes escenas, generación procedural simple y un loop completo del juego."));
+    panel.appendChild(p2);
+    panel.appendChild(p2_2);
+
+    let t3 = document.createElement("h3");
+    t3.appendChild(document.createTextNode("2D Soulslike (3ra imágen)"));
+    panel.appendChild(t3);
+    let p3 = document.createElement("p");
+    p3.appendChild(document.createTextNode("Prototipo realizado para aprender a implementar mecánicas populares de videojuegos plataformeros, soulslike y metroidvania, inspirado en Blasphemous."));
+    panel.appendChild(p3);
+
+    let t4 = document.createElement("h3");
+    t4.appendChild(document.createTextNode("Snake clon (4ta imágen)"));
+    panel.appendChild(t4);
+    let p4 = document.createElement("p");
+    p4.appendChild(document.createTextNode("Prototipo realizado para aprender. :P"));
+    panel.appendChild(p4);
+}
 
 function displayInfo(panelElem, info){
     let elems = Object.keys(info)
